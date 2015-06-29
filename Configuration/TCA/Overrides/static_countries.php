31f7aeb38befbcc459173bb59b1dd8cbc925c816
Tim Lochmüller <tim.lochmueller@hdnet.de>
2015-06-29 10:35:34 +0200

    Fix language label

--- Configuration/TCA/Overrides/static_countries.php
+++ Configuration/TCA/Overrides/static_countries.php
@@ -7,7 +7,7 @@ $additionalFields = array(
 foreach ($additionalFields as $sourceField => $destField) {
 	$additionalColumns = array();
 	$additionalColumns[$destField] = $GLOBALS['TCA']['static_countries']['columns'][$sourceField];
-	$additionalColumns[$destField]['label'] = 'LLL:EXT:static_info_tables_de/Resources/Private/Language/locallang_db.xlf:static_countries_item.' . $destField;
+	$additionalColumns[$destField]['label'] = 'LLL:EXT:static_info_tables_zh/Resources/Private/Language/locallang_db.xlf:static_countries_item.' . $destField;
 	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('static_countries', $additionalColumns);
 	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('static_countries', $destField, '', 'after:' . $sourceField);
 	// Add as search field
