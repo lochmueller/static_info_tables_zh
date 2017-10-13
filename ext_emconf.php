a0228c737e2e8ff49166c4a928dfda496c1e78cd
Tim Lochmüller <tim.lochmueller@hdnet.de>
2017-10-13 12:59:29 +0200

    Fix #3 - Add compatibility for 8.x also

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,7 +22,7 @@ $EM_CONF[$_EXTKEY] = array(
     'author_company'   => '',
     'constraints'      => array(
         'depends' => array(
-            'typo3'              => '6.2.0-7.99.99',
+            'typo3'              => '6.2.0-8.7.99',
             'static_info_tables' => '6.2.0-0.0.0',
         ),
     ),
