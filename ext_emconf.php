de69d38e13db89de18b3e7210bfae88a566f4d86
Tim Lochmüller <tim@fruit-lab.de>
2020-10-20 17:13:04 +0200

    Add composer.lock and drop PHP 7.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = array(
     'author_company'   => '',
     'constraints' => array(
         'depends' => array(
-            'php' => '7.1.0-7.4.99',
+            'php' => '7.2.0-7.4.99',
             'typo3' => '9.5.0-10.4.99',
             'static_info_tables' => '6.9.0-6.9.99',
         ),
