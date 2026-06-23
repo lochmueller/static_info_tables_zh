4b7c379001abd01f753a64639c3010acd084abf3
Stefan Neufeind <github@stefan-neufeind.de>
2026-06-23 11:39:28 +0200

    compat for TYPO3 v13

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@ $EM_CONF[$_EXTKEY] = [
     'title'            => 'Static Info Tables (zh)',
     'description'      => 'Chinese (zh) language pack for the Static Info Tables providing localized names for countries, currencies and so on. Dev on: https://github.com/lochmueller/static_info_tables_zh',
     'category'         => 'misc',
-    'version' => '8.0.0',
+    'version'          => '9.0.0',
     'state'            => 'stable',
     'clearcacheonload' => true,
     'author'           => 'Tim Lochmüller',
@@ -12,9 +12,9 @@ $EM_CONF[$_EXTKEY] = [
     'author_company'   => '',
     'constraints' => [
         'depends' => [
-            'php' => '8.1.0-8.3.99',
-            'typo3' => '12.4.0-12.4.99',
-            'static_info_tables' => '12.4.0-12.4.99',
+            'php' => '8.1.0-8.4.99',
+            'typo3' => '12.4.0-13.4.99',
+            'static_info_tables' => '12.4.0-13.4.99',
         ],
     ],
 ];
