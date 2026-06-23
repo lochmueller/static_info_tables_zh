ba515fd922ec98d3fa9cff2be52f9359e0f9ec43
Tim Lochmüller <tim.lochmueller@hdnet.de>
2026-06-23 13:11:54 +0200

    feat: Support new versions

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # TYPO3 extension static_info_tables_zh
 
-[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/lochmueller/static_info_tables_zh/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/lochmueller/static_info_tables_zh/?branch=master)
-
 ## What is does
 
 Use this extension to get the simplified chinese namings for countries and territories.
--- ext_emconf.php
+++ ext_emconf.php
@@ -12,9 +12,9 @@ $EM_CONF[$_EXTKEY] = [
     'author_company'   => '',
     'constraints' => [
         'depends' => [
-            'php' => '8.1.0-8.3.99',
-            'typo3' => '12.4.0-12.4.99',
-            'static_info_tables' => '12.4.0-12.4.99',
+            'php' => '8.2.0-8.5.99',
+            'typo3' => '12.4.0-13.4.99',
+            'static_info_tables' => '12.4.0-13.4.99',
         ],
     ],
 ];
